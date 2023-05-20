c94b15bff8cb098c3037abd2402b42d717e40036
D0dzs <51715950+D0dzs@users.noreply.github.com>
2023-05-20 15:30:28 +0200

    Kinézet megírása, kész navbar, magyarázat

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("Szoszi van, báttya! 👌")
+const accordion = document.querySelector(".nav");
+
+accordion.addEventListener("click", (e) => {
+  if (window.innerWidth > 900) return;
+
+  const activePanel = e.target.closest(".nav-panel");
+  if (!activePanel) return;
+  toggleAccordion(activePanel);
+});
+
+function toggleAccordion(panelToActivate) {
+  const activeButton = panelToActivate.querySelector("button");
+  const activePanel = panelToActivate.querySelector(".nav-content");
+  const activePanelIsOpened = activeButton.getAttribute("aria-expanded");
+
+  if (activePanelIsOpened === "true") {
+    panelToActivate
+      .querySelector("button")
+      .setAttribute("aria-expanded", false);
+
+    panelToActivate
+      .querySelector(".nav-content")
+      .setAttribute("aria-hidden", true);
+  } else {
+    panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+
+    panelToActivate
+      .querySelector(".nav-content")
+      .setAttribute("aria-hidden", false);
+  }
+}
